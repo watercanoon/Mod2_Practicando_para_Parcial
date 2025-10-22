5e6c620c84788fffc905202f9f343dd9551477c7
LOWIS <ASUS@192.168.11.105>
2025-10-21 21:44:09 -0500

    COLA

---
+++ src/main/java/pe/faustino/mod2_practicando_para_parcial/Cola.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.faustino.mod2_practicando_para_parcial;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cola {
+    
+}
